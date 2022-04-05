9cb9b19d174caee3c9f99bf393cb408598ae0b7f
AaronLyons1 <lyonsaaron02@gmail.com>
2022-04-05 18:54:15 +0000

    Started svd

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 hidden_board = [{' '} * 8 for x in range(8)]
 player_board = [{' '} * 8 for x in range(8)]
 
-letters_numbers = {'A': 0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+letters_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
-def board():
+def start_board(board):
     """
     This function will lay out the board and make it visible
     """
-    pass
+    print('A B C D E F G H')
+    print('  ')
+    row_num = 1
 
 def ship():
     """
@@ -26,3 +28,10 @@ def hit_ship():
     """
     This function will recognise if a ship was hit
     """
+    pass
+
+
+ship()
+
+turns = 10
+while turns > 0:
