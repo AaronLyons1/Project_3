3abae1f3c546a3773ef57fee9dfa3ad881bfe20f
AaronLyons1 <lyonsaaron02@gmail.com>
2022-04-05 18:12:27 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+hidden_board = [{' '} * 8 for x in range(8)]
+player_board = [{' '} * 8 for x in range(8)]
+
+letters_numbers = {'A': 0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
+
+def board():
+    """
+    This function will lay out the board and make it visible
+    """
+    pass
+
+def ship():
+    """
+    This function will make the ships
+    """
+    pass
+
+def where_is_ship():
+    """
+    This will ask the player for thier inputs and input them
+    """
+    pass
+
+def hit_ship():
+    """
+    This function will recognise if a ship was hit
+    """
