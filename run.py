e30b41789f7773198642a2ab3956c1b8b42285aa
AaronLyons1 <lyonsaaron02@gmail.com>
2022-04-08 19:41:35 +0000

    fixed ALL errors that were coming up, finish code

--- run.py
+++ run.py
@@ -1,21 +1,19 @@
 from random import randint
 
-hidden_board = [[" "] * 8 for x in range(8)]
-player_board = [[" "] * 8 for x in range(8)]
+hidden_board = [[" "] * 6 for x in range(6)]
+player_board = [[" "] * 6 for x in range(6)]
 
-letters_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
-
-score = 0
+letters_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5,}
 
 def start_board(board):
     """
     This function will lay out the board and make it visible
     """
-    print("  A B C D E F G H")
-    print("  +-+-+-+-+-+-+-+")
+    print("  A B C D E F")
+    print("  +-+-+-+-+-+")
     row_num = 1
-    for row in board:
-        print("%d|%s|" % (row_num, "|".join(row)))
+    for x_axis in board:
+        print("%d|%s|" % (row_num, "|".join(x_axis)))
         row_num += 1
 
 def ships(board):
@@ -23,59 +21,60 @@ def ships(board):
     This function will make the ships
     """
     for ship in range(5):
-         ship_x, ship_y = randint(0, 7), randint(0, 7)
+        ship_y, ship_x = randint(0, 5), randint(0, 5)
+        while board[ship_y][ship_x] == "X":
+            ship_y, ship_x = randint(0, 5), randint(0, 5)
 
-    while board[ship_x][ship_y] == "X":
-        ship_x, ship_y = randint(0, 7), randint(0, 7)
-
-    board[ship_x][ship_y] = 'X'
+    board[ship_y][ship_x] = 'X'
 
 def where_is_ship():
     """
     This will ask the player for thier inputs and input them
     """
-    y_axis = input("Enter a number ranging from 1-8: ")
-    while y_axis not in '12345678':
+    x_axis = input("Enter a number ranging from 1-8: ")
+    while x_axis not in '12345678':
         print('Invalid, Please enter a valid row number')
-        y_axis = input("Enter a number ranging from 1-8: ")
+        x_axis = input("Enter a number ranging from 1-8: ")
 
-    x_axis = input("Enter a letter ranging from A-H: ").upper()
-    while x_axis not in 'ABCDEFGH':
+    y_axis = input("Enter a letter ranging from A-H: ").upper()
+    while y_axis not in 'ABCDEFGH':
         print('Invalid, Please enter a valid letter')
-        x_axis = input("Enter a letter ranging from A-H: ").upper()
+        y_axis = input("Enter a letter ranging from A-H: ").upper()
          
-    return int(y_axis) -1, letters_numbers(x_axis) #Reason for -1 is because its technically 1 on the board but its 0 in our list
+    return int(x_axis) -1, letters_numbers[y_axis]
 
-def hit_ship():
+def hit_ship(board):
     """
     This function will recognise if a ship was hit
     """
     score = 0
     for y_axis in board:
         for x_axis in y_axis:
-            if x_axis == 'X':
+            if x_axis == "X":
                 score += 1
+    return score
 
-if __name__ == "__main__": #This line is used to allow or prevent parts of code from being run when the modules are imported
+if __name__ == "__main__": #This line is used to allow or prevent parts of code from being run when the modules are imported           
     ships(hidden_board)
-    turns = 15
+    turns = 10
     while turns > 0:
         print("Where do you think the ships are?")
         start_board(player_board)
         y_axis, x_axis = where_is_ship()
-        if hidden_board[x_axis][y_axis] == "x":
-            print("Hit")
-            player_board[x_axis][y_axis] = "X"
-            turns -= 1
-        elif player_board[x_axis][y_axis] == "-":
+        if player_board[y_axis][x_axis] == "0":
             print("You have already hit that location")
+        elif hidden_board[y_axis][x_axis] == "X":
+            print("Hit")
+            player_board[y_axis][x_axis] = "X" 
+            turns -= 1  
         else:
-            print("MISS")
-            player_board[x_axis][y_axis] = "-"
-            turns -= 1
-        if ships(player_board) == 5:
+            print("MISS!")
+            player_board[y_axis][x_axis] = "0"   
+            turns -= 1     
+        if hit_ship(player_board) == 5:
             print("You Win")
             break
+        print(f"You have {turns} turns left")
         if turns == 0:
             print("Sorry you have run out of tries")
 
